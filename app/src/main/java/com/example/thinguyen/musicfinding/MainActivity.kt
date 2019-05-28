4a6dd67f7b6ffb4ce3828f060a2a1c8e03878dae
thinguyenwizeline <thi.nguyen@wizeline.com>
2019-05-28 15:06:44 +0700

    Check username and password

--- app/src/main/java/com/example/thinguyen/musicfinding/MainActivity.kt
+++ app/src/main/java/com/example/thinguyen/musicfinding/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 /**
  * Created by thinguyen on 26/05/2019.
@@ -13,18 +15,47 @@ import android.widget.Button
 class MainActivity : AppCompatActivity() {
 
     private var loginButton: Button? = null
+    private var editTextUserName: EditText? = null
+    private var editTextPassword: EditText? = null
+    private var userName: String? = null
+    private var password: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         loginButton = findViewById<View>(R.id.loginButton) as Button
+        editTextUserName = findViewById<View>(R.id.edEmail) as EditText
+        editTextPassword = findViewById<View>(R.id.edPassword) as EditText
 
         loginButton!!.setOnClickListener { openMainMenuPage() }
     }
 
     fun openMainMenuPage() {
-        val mainMenuIntent = Intent(this, MenuPageActivity::class.java)
-        startActivity(mainMenuIntent)
+        var checkValidation = false;
+        userName = editTextUserName!!.text.toString();
+        password = editTextPassword!!.text.toString();
 
+        checkValidation = checkUserNamePassword(userName.toString(),password.toString());
+
+        if(checkValidation == true){
+            val mainMenuIntent = Intent(this, MenuPageActivity::class.java)
+            startActivity(mainMenuIntent)
+        }
+        else{
+            Toast.makeText(getApplicationContext(),"User Name or Password incorrect",Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+/**
+ * Check validation for user name and password
+ * username > 6 characters
+ * password > 8 characters*/
+
+    fun checkUserNamePassword(userNameText:String , passwordText:String) :Boolean {
+        if(userNameText.length <= 6 || passwordText.length <= 8){
+            return false;
+        }
+        return true;
     }
 }
--- app/src/main/java/com/example/thinguyen/musicfinding/MenuPageActivity.kt
+++ app/src/main/java/com/example/thinguyen/musicfinding/MenuPageActivity.kt
@@ -3,9 +3,7 @@ package com.example.thinguyen.musicfinding
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
-import android.view.MenuItem
 import android.widget.TextView
-import android.view.View
 
 /**
  * Created by thinguyen on 26/05/2019.
